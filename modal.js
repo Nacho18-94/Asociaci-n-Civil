248c740930556168c0386764dfdb9d01fe99b6d9
Nacho18-94 <93226888+Nacho18-94@users.noreply.github.com>
2024-05-29 16:30:55 -0300

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,3 +1,4 @@
+//Esto corresponde al Modal
 //variable
 cover_img = document.getElementById("ctn-cover-img");
 cover_img2 = document.getElementById("ctn-cover-img2");
@@ -21,3 +22,15 @@ document.getElementById("btn-img").addEventListener("click", mostrar_img);
 document.getElementById("btn-img2").addEventListener("click", mostrar_img2);
 document.getElementById("cerrar-img").addEventListener("click", ocultar_img);
 document.getElementById("cerrar-img2").addEventListener("click", ocultar_img2);
+
+//Esto corresponde al Header
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
